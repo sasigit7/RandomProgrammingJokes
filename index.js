6f8122d7673aace6d67a08d263f0cb9edb34d6bc
shashidar mandalozu <sasitech18@gmail.com>
2020-10-12 20:12:56 +0100

    Created Required Variables

---
+++ index.js
@@ -0,0 +1,8 @@
+// Create 4 variables and assign them to their respective elements: setupDiv,
+// punchlineDiv, punchlineBtn, newJokeBtn
+
+const setupDiv = document.getElementById('setup');
+const punchlineDiv = document.getElementById('punchline');
+const punchlineBtn = document.getElementById('punchlineBtn');
+const newJokeBtn = document.getElementById('newJokeBtn');
+
