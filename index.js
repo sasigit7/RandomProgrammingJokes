063380efd10d3b50ea61f4f7515b64f948549890
shashidar mandalozu <sasitech18@gmail.com>
2020-10-12 20:15:58 +0100

    Setup an async function called getJoke()

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ const punchlineDiv = document.getElementById('punchline');
 const punchlineBtn = document.getElementById('punchlineBtn');
 const newJokeBtn = document.getElementById('newJokeBtn');
 
+// Setup an async function called getJoke Create a variable called jokePromise
+// that fetches a joke from
+// https://official-joke-api.appspot.com/jokes/programming/random create a
+// variable called joke that consumes the json data
+
+const getJoke = async() => {
+    const jokePromise = await fetch('https://official-joke-api.appspot.com/jokes/programming/random');
+    const joke = await jokePromise.json();
+
+    console.log(joke);
+}
+getJoke();
